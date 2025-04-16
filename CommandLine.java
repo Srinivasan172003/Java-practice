24f35f9783f278c5bc21bc1bfa0a6df37b7737bb
SRINIVASAN <srini172003@gmail.com>
2025-04-16 08:22:13 +0530

    Command Line arguments
    This program get a input from the user in command line with out storing the value in any variables

---
+++ CommandLine.java
@@ -0,0 +1,11 @@
+package commandline;
+
+public class CommandLine {
+	public static void main(String[] args) {
+		
+		for(int i=0;i< args.length;i++) {
+		System.out.println("ENTER THE VALLUE TO ENTER WITHIN COMMAND LINE :"+ args);
+		//will execute the value through command line
+	}
+	}
+}
